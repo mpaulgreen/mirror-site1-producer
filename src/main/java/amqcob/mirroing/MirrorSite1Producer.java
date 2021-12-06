10b20131d5cd1e5d85ef9d5e6f57650a143d6e5f
Mriganka Paul <mpaul@redhat.com>
2021-12-06 08:35:06 -0500

    Corrected the package structure

--- src/main/java/amqcob/mirroing/MirrorSite1Producer.java
+++ src/main/java/amqcob/mirroing/MirrorSite1Producer.java
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package mirroing;
+package amqcob.mirroing;
 
 import org.apache.qpid.jms.JmsConnectionFactory;
 
